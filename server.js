bfcd6c55fcfa5f29727c26501f0764b385756fa8
Kelechi Ebiri <ebiritg@gmail.com>
2026-01-18 10:16:38 +0100

    feat: enhance environment configuration and database connection handling

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
-const PORT = env.PORT || process.env.PORT;
+import env from "./config/env.js";
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+import express from "express";
+import router from "./routes/index.js";
+import RedisClient from "./utils/redis.js";
+import DBClient from "./utils/db.js";
+
+console.log("ENV LOADED:", process.env.DB_USER);
+
+const app = express();
+
+const dbClient = new DBClient({
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  database: env.DB_NAME,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
 });
+
+await dbClient.connect();
+
+const redisClient = new RedisClient();
+app.use(express.json({ limit: "50mb" }));
+app.use("/", router);
+app.get("/health", async (_, res) => {
+  try {
+    await dbClient.query("SELECT 1");
+    res.status(200).json({ status: "ok" });
+  } catch {
+    res.status(500).json({ status: "db down" });
+  }
+});
+
+app.listen(env.PORT, () => {
+  console.log(`Server running on ${env.PORT}`);
+});
+
+export { dbClient, redisClient };
--- utils/db.js
+++ utils/db.js
@@ -1,34 +1,36 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+/**
+ * Represents a Postgres client for interacting with the database.
+ */
 class DBClient {
   constructor() {
-    if (process.env.DATABASE_URL) {
-      this.pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      });
-    } else {
-      this.pool = new Pool({
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-      });
-    }
+    this.pool = new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
   }
 
+  /**
+   * Connects to the Postgres database.
+   */
   async connect() {
     try {
       const client = await this.pool.connect();
       console.log("Connected to Pg database");
       client.release();
     } catch (error) {
-      console.error("Failed to connect to database");
-      console.error(error);
+      console.error("Failed to connect to database:", error.message);
       process.exit(1);
     }
+  }
+
+  async disconnect() {
+    await this.pool.end();
   }
 
   query(text, params) {
